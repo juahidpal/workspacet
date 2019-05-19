f6e29fa249359524d19cdf965c5d51a280a5ff7b
luicahleo <luisrolandocahuanaleon@gmail.com>
2019-05-19 11:01:16 +0200

    continuamos

--- bloc/bbdd/tablas.sql
+++ bloc/bbdd/tablas.sql
@@ -40,11 +40,11 @@ CREATE TABLE IF NOT EXISTS notas (
 -- Insercion de datos de ejemplo
 --
 INSERT INTO usuarios VALUES('usuario','clave',1);
-INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen) VALUES('usuario','Nota de prueba 1','Esta es la nota 1','../imagenes/nota.png');
-INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen) VALUES('usuario','Nota de prueba 2','Esta es la nota 2','../imagenes/nota2.png');
-INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen) VALUES('usuario','Nota de prueba 3','Esta es la nota 3','../imagenes/nota2.png');
-INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen) VALUES('usuario','Nota de prueba 4','Esta es la nota 4','../imagenes/nota2.png');
-INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen) VALUES('usuario','Nota de prueba 5','Esta es la nota 5','../imagenes/nota2.png');
+INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen,categoria,color) VALUES('usuario','Nota de prueba 1','Esta es la nota 1','../imagenes/nota.png','FAST','DARKSIDE');
+INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen,categoria,color) VALUES('usuario','Nota de prueba 2','Esta es la nota 2','../imagenes/nota2.png','FAST','DARKSIDE');
+INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen,categoria,color) VALUES('usuario','Nota de prueba 3','Esta es la nota 3','../imagenes/nota2.png','FAST','DARKSIDE');
+INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen,categoria,color) VALUES('usuario','Nota de prueba 4','Esta es la nota 4','../imagenes/nota2.png','FAST','DARKSIDE');
+INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen,categoria,color) VALUES('usuario','Nota de prueba 5','Esta es la nota 5','../imagenes/nota2.png','FAST','DARKSIDE');
 INSERT INTO usuarios VALUES('admin','clave',0);
-INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen) VALUES('admin','Nota de prueba 6','Esta es la nota 6','../imagenes/nota2.png');
+INSERT INTO notas (nombre_usuario, titulo, nota, urlimagen,categoria,color) VALUES('admin','Nota de prueba 6','Esta es la nota 6','../imagenes/nota2.png','FASTadmin','WHITESIDE');
 
